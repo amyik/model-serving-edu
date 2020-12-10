0418d60ceff1e0e0960b5aca8092116082bd3ea7
albert <mm.yoon@samsung.com>
2020-12-10 19:17:13 +0900

    u

--- covid-fastapi/main.py
+++ covid-fastapi/main.py
@@ -377,7 +377,7 @@ async def covid_classifier_model2(request: Request):
 
     #MODEL2_API_URL is tensorflow serving URL in another docker
     HEADERS = {'content-type': 'application/json'}
-    MODEL2_API_URL = 'http://127.0.0.1:8511/v1/models/covid19/versions/2:predict'
+    MODEL2_API_URL = 'http://34.97.79.121:8511/v1/models/covid19:predict'
     CLASS_NAMES = ['Covid19', 'Normal_Lung', 'Pneumonia_Bacterial_Lung']
 
     json_response = requests.post(MODEL2_API_URL, data=data, headers=HEADERS)
@@ -418,7 +418,7 @@ async def covid_classifier_model2_heatmap(request: Request):
 
     #MODEL2_API_URL is tensorflow serving URL in another docker
     HEADERS = {'content-type': 'application/json'}
-    MODEL2_API_URL = 'http://127.0.0.1:8511/v1/models/covid19/versions/2:predict'
+    MODEL2_API_URL = 'http://34.97.79.121:8511/v1/models/covid19:predict'
     CLASS_NAMES = ['Covid19', 'Normal_Lung', 'Pneumonia_Bacterial_Lung']
 
     json_response = requests.post(MODEL2_API_URL, data=data, headers=HEADERS)
