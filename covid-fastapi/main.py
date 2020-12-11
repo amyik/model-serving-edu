48e6cd02e8f362e673f4f21c0c730da42bec60e1
albert <mm.yoon@samsung.com>
2020-12-11 09:25:39 +0900

    u

--- covid-fastapi/main.py
+++ covid-fastapi/main.py
@@ -379,7 +379,8 @@ async def covid_classifier_model2(request: Request):
     # HEADERS = {'content-type': 'application/json', 'Host': 'covid19.myspace.example.com'}
     # MODEL2_API_URL = 'http://kfserving-ingressgateway.istio-system:80/v1/models/covid19:predict'
     HEADERS = {'content-type': 'application/json'}
-    MODEL2_API_URL = 'http://34.97.79.121:8511/v1/models/covid19:predict'
+    # MODEL2_API_URL = 'http://34.97.79.121:8511/v1/models/covid19:predict'
+    MODEL2_API_URL = 'http://kfserving-ingressgateway.istio-system:80/v1/models/covid19:predict'
     # MODEL2_API_URL = 'http://34.97.79.121:8511/v1/models/covid19:predict'
     CLASS_NAMES = ['Covid19', 'Normal_Lung', 'Pneumonia_Bacterial_Lung']
 
